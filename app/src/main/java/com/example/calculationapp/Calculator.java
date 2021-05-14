436b4ca27f9baf2ece51d389cb5b8ccfa3a5c9a8
systemry420 <systemry420@gmail.com>
2021-05-14 16:31:42 +0300

    change layout and buttons to hard-coded

--- app/src/main/java/com/example/calculationapp/Calculator.java
+++ app/src/main/java/com/example/calculationapp/Calculator.java
@@ -9,38 +9,19 @@ import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
 
 import com.google.android.material.button.MaterialButton;
 
 public class Calculator extends AppCompatActivity {
-    GridLayout gridLayout;
+    LinearLayout buttonsLayout;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        gridLayout = findViewById(R.id.buttonsGrid);
 
-        for (int i = 9; i >= 0; i--) {
-            MaterialButton btn = new MaterialButton(this);
-            btn.setText(String.valueOf(i));
-            btn.setId(i);
-            GridLayout.LayoutParams params;
-            if(i == 0) {
-//              params = new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, 1f), GridLayout.spec(GridLayout.UNDEFINED, 2f));
-                continue;
-            } else {
-                params = new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, 1f), GridLayout.spec(GridLayout.UNDEFINED, 1f));
-            }
-            params.height = 0;
-            params.width = 0;
-            btn.setTextSize(24);
-            btn.setLayoutParams(params);
-            btn.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
-            btn.setTextColor(Color.rgb(88, 88, 88));
-            gridLayout.addView(btn);
-        }
     }
 
 //    public void onClick(View v) {
